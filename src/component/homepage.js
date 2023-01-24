9a4dbbad25c5f281c3fa63506fd95d38073030a2
treva556 <marktreva92@gmail.com>
2023-01-24 14:33:03 +0300

    edited

--- src/component/homepage.js
+++ src/component/homepage.js
@@ -102,16 +102,16 @@ class DesktopContainer extends Component {
                 <Menu.Item as='a' active>
                   Home
                 </Menu.Item>
-                <Menu.Item as='a' active>Organizations</Menu.Item>
-               {/* <Menu.Item as='a'>Company</Menu.Item>
-    <Menu.Item as='a'>Careers</Menu.Item>*/}
+               {/* <Menu.Item as='a' active>Organizations</Menu.Item>*/}
+               <Menu.Item as='a' active>Organizations</Menu.Item>
+   {/* <Menu.Item as='a'>Careers</Menu.Item>*/}
                 <Menu.Item position='right'>
                   <Button as='a' inverted={!fixed}>
-                    Log out
+                    Search
                   </Button>
-                 {/* <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    Sign Up
-</Button>*/}
+                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
+                    Log Out
+</Button>
                 </Menu.Item>
               </Container>
             </Menu>
@@ -157,8 +157,8 @@ class MobileContainer extends Component {
             {/*<Menu.Item as='a'>Work</Menu.Item>
             <Menu.Item as='a'></Menu.Item>
     <Menu.Item as='a'>Careers</Menu.Item>*/}
-            <Menu.Item as='a'>Log in</Menu.Item>
-            <Menu.Item as='a'>Sign Up</Menu.Item>
+            <Menu.Item as='a'>Search</Menu.Item>
+            <Menu.Item as='a'>Log Out</Menu.Item>
           </Sidebar>
 
           <Sidebar.Pusher dimmed={sidebarOpened}>
@@ -175,10 +175,10 @@ class MobileContainer extends Component {
                   </Menu.Item>
                   <Menu.Item position='right'>
                     <Button as='a' inverted>
-                      Log in
+                      Search
                     </Button>
                     <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                      Sign Up
+                      Log Out
                     </Button>
                   </Menu.Item>
                 </Menu>
@@ -216,7 +216,7 @@ ResponsiveContainer.propTypes = {
 const HomepageLayout = () => (
   <ResponsiveContainer>
     <Segment style={{ padding: '8em 0em' }} vertical>
-      <Grid container stackable verticalAlign='middle'>
+      {/* <Grid container stackable verticalAlign='middle'>
         <Grid.Row>
           <Grid.Column width={8}>
             <Header as='h3' style={{ fontSize: '2em' }}>
@@ -235,7 +235,7 @@ const HomepageLayout = () => (
           </Grid.Column>
           {/*<Grid.Column floated='right' width={6}>
             <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
-</Grid.Column>*/}
+</Grid.Column>*
         </Grid.Row>
         <Grid.Row>
           <Grid.Column textAlign='center'>
@@ -261,10 +261,19 @@ const HomepageLayout = () => (
            {/* <p style={{ fontSize: '1.33em' }}>
               <Image avatar src='/images/avatar/large/nan.jpg' />
               <b>Nan</b> Chief Fun Officer Acme Toys
-</p>*/}
+</p>*
           </Grid.Column>
         </Grid.Row>
-      </Grid>
+</Grid> */}
+
+
+
+
+Animal collection comes here
+
+
+
+
     </Segment>
 
   {/*fooooter*/}
@@ -282,15 +291,15 @@ const HomepageLayout = () => (
                 <List.Item as='a'>Gazebo Plans</List.Item>
               </List>
             </Grid.Column>
-            <Grid.Column width={3}>
+          {/*  <Grid.Column width={3}>
               <Header inverted as='h4' content='Services' />
               <List link inverted>
-                <List.Item as='a'>Banana Pre-Order</List.Item>
+                <List.Item as='a'>Banana</List.Item>
                 <List.Item as='a'>DNA FAQ</List.Item>
                 <List.Item as='a'>How To Access</List.Item>
                 <List.Item as='a'>Favorite X-Men</List.Item>
-              </List>
-            </Grid.Column>
+</List>
+</Grid.Column>*/}
             <Grid.Column width={7}>
               <Header as='h4' inverted>
                 Footer Header
