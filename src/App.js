3e3ecd41755bab66551064617f4fb5d5f91f8c3b
Sammy-CK <sammycherono2696@gmail.com>
2023-01-25 08:59:57 +0300

    Added animal and categories page

--- src/App.js
+++ src/App.js
@@ -14,12 +14,19 @@ function App() {
   const [type, setType] = useState("dog");
   const [currentBreeds, setCurrentBreeds] = useState([]);
   const [searchValue, setSearchValue] = useState("");
+  const [searchedResult, setSearchedResult] = useState([]);
+
+
+
+
+  //   setSearchedResult(animals.filter((animal) =>
+  //   animal.breeds.primary.toLowerCase().includes(searchValue.toLowerCase())
+  // ))
 
   // curl -d "grant_type=client_credentials&client_id=TBUWjfxlzXKspuMgyLyTavDjNWaj0hrPRy1x3nFBACqlc6swLs&client_secret=n3x8Phb9OYqLL1rdWtaJFdtn3KUgAmUzYyauP9yz" https://api.petfinder.com/v2/oauth2/token
 
   const token =
-    "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJUQlVXamZ4bHpYS3NwdU1neUx5VGF2RGpOV2FqMGhyUFJ5MXgzbkZCQUNxbGM2c3dMcyIsImp0aSI6IjMzZGYyNzE2YmFjYjQ2Yjg5ZGNhZjIyMWZjNDIzYzlkNmEzY2UzZDIxMzVmZTBiZDA2MmQwZWYyNWVjNzk0OThmY2FkY2U0YjRjYjEzZmYzIiwiaWF0IjoxNjc0NjIxNTE4LCJuYmYiOjE2NzQ2MjE1MTgsImV4cCI6MTY3NDYyNTExOCwic3ViIjoiIiwic2NvcGVzIjpbXX0.sZLur45BqR5mwMfHJfR30cjaghtF0oSgdXXslJef72tjUqNmGD4siYPOAU1HGniPaDtAE1COJIGXfMxIfpu1XsqKi_MghjHKt_O6h-ky8yuqm80SAP_lRiFhZ5Sno4RgkiPGwGWKldi_1A6gLYUKXh9D_V4cwjlgXwZlXg9xgUkyGav_v56VfoKB_MC8xIotaW8RvN1_kY0MO7m6dyr-944UgK1cK8YOyWkThAenDPHCS95VbdCYyL2WugaOMZQazkMqofIRZsZ-B8I_M5LeYUIRcFyCad6h_m0HjN9WtWeGOUM_KUWgC7OUQhQZv9S8eNrOWsHU9s-P-Gj_8yBPFA";
-
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJUQlVXamZ4bHpYS3NwdU1neUx5VGF2RGpOV2FqMGhyUFJ5MXgzbkZCQUNxbGM2c3dMcyIsImp0aSI6ImRmYTgzYjU0M2Y5NzAxMzA3YjcxOTViNzBmOGNmYjRhYWQ4ZDlkZjQ2NDRmOWI3M2U2ZjU2YjRjYmYwMDc1MWFlMmUxOWVlMTQ3YjE0ODZhIiwiaWF0IjoxNjc0NjI1ODc2LCJuYmYiOjE2NzQ2MjU4NzYsImV4cCI6MTY3NDYyOTQ3Niwic3ViIjoiIiwic2NvcGVzIjpbXX0.X-RZyTGk9Qi6VdB550v_cdvy-L890iL_GYrW8wpw4kFFzHmFesZm-wmOyji40I5JkKMOOfXRQOiNSfww8M3uMKtdEPs-EtTQMCk4_VhNr4pTsbUictJjBatpmz58atsWYAQG7S8XyAYkRqHeAgMbPAzq3kL5D1MJWQDyceJReTbtIKZC5JsjpLCdxWU3yN-NTO-f-n6naAT7gyU3aCX0_TL4TyNClGuBg6Gg2qQBQ7LlHJesAR9EyrJ6WxaBl-J6UHJVYg533S3R_bEOOsDYQM7PmDNG2J0MG-4QOfdD2-m__SIX9GNcPhRFHPn8xOBPbFEBuFygFcp6f8J6ivI-0Q"
 
   useEffect(() => {
     //Animals
@@ -32,7 +39,7 @@ function App() {
     })
       .then((resp) => resp.json())
       .then((data) => {
-        // console.log(data);
+        console.log(data);
         setAnimals(data);
       });
 
