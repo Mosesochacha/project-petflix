d73e7682348366fc53f4e075fffcecf44d337d2b
Sammy-CK <sammycherono2696@gmail.com>
2023-01-26 20:48:11 +0300

    Added styling

--- src/component/homepage.js
+++ src/component/homepage.js
@@ -1,312 +1,98 @@
+import React from "react";
+import { Link as NavLink } from "react-router-dom";
+import "./homepage.css";
 
-import { createMedia } from '@artsy/fresnel'
-import PropTypes from 'prop-types'
-import React, { Component } from 'react'
+function HomepageLayout() {
+  return (
+    <div className="landingDiv">
+
+<NavLink className="homepageLinks" to="/login">LOG IN</NavLink>
+      <NavLink className="homepageLinks" to="/register">SIGN UP</NavLink>
+
+      <h1 style={{textAlign: "left", padding: "0px 10px 10px 10px", fontSize:"4vw", fontFamily:"fantasy", width:"40%"}}>PETFLIX</h1>
 
 
-import {
-  Button,
-  Container,
-  Dropdown,
-  //Divider,
-  Grid,
-  Header,
-  Icon,
-  //Image,
-  List,
-  Menu,
-  Segment,
-  Sidebar,
-  Visibility,
-} from 'semantic-ui-react'
+      <div style={{textAlign:"right", paddingRight:"30px"}}>
+      <p><b style={{fontSize:"2vw"}}>A humane, compassionate future for every animal friend.</b></p>
+      </div>
 
-const { MediaContextProvider, Media } = createMedia({
-  breakpoints: {
-    mobile: 0,
-    tablet: 768,
-    computer: 1024,
-  },
-})
+      <div className="insideHomePage">
 
-/* Heads up!
- * HomepageHeading uses inline styling, however it's not the best practice. Use CSS or styled
- * components for such things.
- */
-const HomepageHeading = ({ mobile }) => (
-  <Container text>
-    <Header
-      as='h1'
-      content='Welcome to Petflix'
-      inverted
-      style={{
-        fontSize: mobile ? '2em' : '4em',
-        fontWeight: 'normal',
-        marginBottom: 0,
-        marginTop: mobile ? '1.5em' : '3em',
-      }}
-    />
-    <Header
-      as='h2'
-      content='Life is Better With Pets.'
-      inverted
-      style={{
-        fontSize: mobile ? '1.5em' : '1.7em',
-        fontWeight: 'normal',
-        marginTop: mobile ? '0.5em' : '1.5em',
-      }}
-    />
-    
-    <Button primary size='huge'>
-      Get Started
-      <Icon name='right arrow' />
-    </Button>
-    </Container>
-)
 
-HomepageHeading.propTypes = {
-  mobile: PropTypes.bool,
+
+      <div className="missionDiv">
+      <p style={{fontWeight:"700", fontFamily:"fantasy"}}>
+        We are an online adoption website who anticipate the future needs of our animal
+        friends—the animals we love and our friends who love animals.
+        We are professional, qualified, and devoted to our organizational
+        duties. ADOPT PLEASE!
+      </p>
+    </div>
+
+    <div className="testUl">
+    <h1>Testimonials</h1>
+
+      <div className="testLi">
+
+          <p>
+          "In January of 2020 I adopted a a tuxedo with a white-tipped tail now known as Roscoe, It was the greatest decision of my life!  What a guy he turned out to be. He follows me all over the house, greets me at the door with a “meow” when I come home, and snuggles in bed sometimes, and I would not have it any other way. He settled in with my nine other indoor cats, all rescues, very easily!"
+          </p>
+          <b>~Peter Wells adopted Roscoe in June 2020.</b>
+
+      </div>
+
+
+      <div className="testLi">
+
+          <p>
+          Thank you for the wonderful experience of adopting Daisy. She has been wonderful. She settled into our journey across the States like a pro, as if she were part of us from the very beginning. Just wanted to let you know that we are truly grateful for how well cared for she was and how much she will be loved here. Lots of hugs!”
+          </p>
+          <b>~Cyndi and Louella Voegeli adopted Daisy in August 2019.</b>
+
+        </div>
+
+
+        <div className="testLi">
+
+          <p>
+          "Over the last two years we've learned a lot from Beaker (like why he was named Rocket while at petflix). We've certainly had some challenges, but he has made tremendous progress and has taught us to be more patient and to enjoy the small things in life. We couldn't be happier! Beaker is a great companion, whether we're running, hiking, or just sitting at home. We can't imagine our lives without him! Thank you for bringing him into our lives."
+          </p>
+          <b>~The Cheryl and Ben Rubinstein adopted Beaker in September 2022</b>
+
+        </div>
+
+
+        <div className="testLi">
+
+          <p>
+          "ThCarole Saxe and Sagecropwebanks to all the caring adoption staff who spent time introducing us to Sage (formerly
+known as Sadie). She's a great little dog! She seems quite happy and we are thrilled to have her."
+          </p>
+          <b>~Sammy Cherono adopted Daisy in December 2021</b>
+        </div>
+
+
+        </div>
+
+        <h2>Our Mission Statement</h2>
+
+
+        <blockquote>
+        To rescue, rehabilitate and rehome animals in crisis, ensure healthy
+        pets through education, advocacy and affordable services, and inspire a
+        community where the animal-human bond is celebrated and nurtured.
+      </blockquote>
+
+<small><b>	&#169; 2022 PETFLIX</b></small>
+
+        </div>
+
+
+
+
+
+    </div>
+  );
 }
 
-/* Heads up!
- * Neither Semantic UI nor Semantic UI React offer a responsive navbar, however, it can be implemented easily.
- * It can be more complicated, but you can create really flexible markup.
- */
-class DesktopContainer extends Component {
-  state = {}
-
-  hideFixedMenu = () => this.setState({ fixed: false })
-  showFixedMenu = () => this.setState({ fixed: true })
-
-  render() {
-    const { children } = this.props
-    const { fixed } = this.state
-
-    return (
-      <Media greaterThan='mobile'>
-        <Visibility
-          once={false}
-          onBottomPassed={this.showFixedMenu}
-          onBottomPassedReverse={this.hideFixedMenu}
-        >
-          <Segment
-            inverted
-            textAlign='center'
-            style={{ minHeight: 700, padding: '1em 0em' }}
-            vertical
-          >
-            <Menu
-              fixed={fixed ? 'top' : null}
-              inverted={!fixed}
-              pointing={!fixed}
-              secondary={!fixed}
-              size='large'
-            >
-              <Container>
-                <Menu.Item position='' as='a' active>
-                  Home
-                </Menu.Item>
-                <Menu.Item position='right'>
-                  <Button as='a' inverted={!fixed}>
-                    Search
-                    <Dropdown/>
-                  </Button>
-                  <Button as='a' inverted={!fixed} primary={fixed} style={{ marginLeft: '0.5em' }}>
-                    Log Out
-</Button>
-                </Menu.Item>
-              </Container>
-            </Menu>
-            <HomepageHeading />
-          </Segment>
-        </Visibility>
-
-        {children}
-      </Media>
-    )
-  }
-}
-
-DesktopContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-class MobileContainer extends Component {
-  state = {}
-
-  handleSidebarHide = () => this.setState({ sidebarOpened: false })
-
-  handleToggle = () => this.setState({ sidebarOpened: true })
-
-  render() {
-    const { children } = this.props
-    const { sidebarOpened } = this.state
-
-    return (
-      <Media as={Sidebar.Pushable} at='mobile'>
-        <Sidebar.Pushable>
-          <Sidebar
-            as={Menu}
-            animation='overlay'
-            inverted
-            onHide={this.handleSidebarHide}
-            vertical
-            visible={sidebarOpened}
-          >
-            <Menu.Item as='a' active>
-              Home
-            </Menu.Item>
-            {/*<Menu.Item as='a'>Work</Menu.Item>
-            <Menu.Item as='a'></Menu.Item>*/}
-    <Menu.Item as='a'>Organizations</Menu.Item>
-            <Menu.Item as='a'>Search</Menu.Item>
-            <Menu.Item as='a'>Log Out</Menu.Item>
-          </Sidebar>
-
-          <Sidebar.Pusher dimmed={sidebarOpened}>
-            <Segment
-              inverted
-              textAlign='center'
-              style={{ minHeight: 350, padding: '1em 0em' }}
-              vertical
-            >
-              <Container>
-                <Menu inverted pointing secondary size='large'>
-                  <Menu.Item onClick={this.handleToggle}>
-                    <Icon name='sidebar' />
-                  </Menu.Item>
-                  <Menu.Item position='right'>
-                   <Dropdown/>
-                    <Button as='a' inverted style={{ marginLeft: '0.5em' }}>
-                      Log Out
-                    </Button>
-                  </Menu.Item>
-                </Menu>
-              </Container>
-              <HomepageHeading mobile />
-            </Segment>
-
-            {children}
-          </Sidebar.Pusher>
-        </Sidebar.Pushable>
-      </Media>
-    )
-  }
-}
-
-MobileContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-const ResponsiveContainer = ({ children }) => (
-  /* Heads up!
-   * For large applications it may not be best option to put all page into these containers at
-   * they will be rendered twice for SSR.
-   */
-  <MediaContextProvider>
-    <DesktopContainer>{children}</DesktopContainer>
-    <MobileContainer>{children}</MobileContainer>
-  </MediaContextProvider>
-)
-
-ResponsiveContainer.propTypes = {
-  children: PropTypes.node,
-}
-
-const HomepageLayout = () => (
-  <ResponsiveContainer>
-    <Segment style={{ padding: '8em 0em' }} vertical>
-      {/* <Grid container stackable verticalAlign='middle'>
-        <Grid.Row>
-          <Grid.Column width={8}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              We Help Companies and Companions
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              We can give your company s
-            </p>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              We Make Bananas That Can Dance
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>
-              Yes that's right, you thought it was the stuff of dreams, but even bananas can be
-              bioengineered.
-            </p>
-          </Grid.Column>
-          {/*<Grid.Column floated='right' width={6}>
-            <Image bordered rounded size='large' src='/images/wireframe/white-image.png' />
-</Grid.Column>*
-        </Grid.Row>
-        <Grid.Row>
-          <Grid.Column textAlign='center'>
-            <Button size='huge'>Check Them Out</Button>
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
-    </Segment>
-
-    <Segment style={{ padding: '0em' }} vertical>
-      <Grid celled='internally' columns='equal' stackable>
-        <Grid.Row textAlign='center'>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "What a Company"
-            </Header>
-            <p style={{ fontSize: '1.33em' }}>That is what they all say about us</p>
-          </Grid.Column>
-          <Grid.Column style={{ paddingBottom: '5em', paddingTop: '5em' }}>
-            <Header as='h3' style={{ fontSize: '2em' }}>
-              "I shouldn't have gone with their competitor."
-            </Header>
-           {/* <p style={{ fontSize: '1.33em' }}>
-              <Image avatar src='/images/avatar/large/nan.jpg' />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-</p>*
-          </Grid.Column>
-        </Grid.Row>
-</Grid> */}
-
-
-
-
-Animal collection comes here
-
-
-
-
-    </Segment>
-
-  {/*fooooter*/}
-
-    <Segment inverted vertical style={{ padding: '3em 0em', textAlign: 'centre' }}>
-      {/* <Container>
-        <div className='container' key={list}>
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as='h4' content='About'/>
-              <List link inverted>
-                <List.Item as='a'></List.Item>
-                <List.Item as='a'>Contact </List.Item>
-                <List.Item as='a'></List.Item>
-                <List.Item as='a'></List.Item>
-              </List>
-            </Grid.Column> */}
-          {/*  <Grid.Column width={3}>
-              <Header inverted as='h4' content='Services' />
-              <List link inverted>
-                <List.Item as='a'>Banana</List.Item>
-                <List.Item as='a'>DNA FAQ</List.Item>
-                <List.Item as='a'>How To Access</List.Item>
-                <List.Item as='a'>Favorite X-Men</List.Item>
-</List>
-</Grid.Column>*/}
-          
-          {/* </Grid.Row>
-        </Grid> */}
-        {/* </div>
-      </Container> */}
-    </Segment>
-  </ResponsiveContainer>
-)
-
-export default HomepageLayout
+export default HomepageLayout;
